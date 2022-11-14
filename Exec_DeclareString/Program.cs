dc740cbb3b2f225b944bfee89efa440c7c028e20
Yun ni <erknard2015@gmail.com>
2022-11-14 23:46:56 +0800

    加入專案檔案。

---
+++ Exec_DeclareString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  //Q1 顯示C:\temp\a.jpg
+            
+            string path = "c:\\temp\\a.jpg";
+            Console.WriteLine(path);
+           
+            string path2 = @"c:\temp\a.jpg";
+            Console.WriteLine(path2);
+
+                //Q2 顯示我是一個"好學生".
+
+            string value = "我是一個\"好學生\".";
+            Console.WriteLine(value);
+
+            string value2 = @"我是一個""好學生"".";
+            Console.WriteLine(value2);
+
+            //Q3 折行顯示我是第N行文字
+
+            string sentence = "我是第一行\r\n我是第二行";
+            Console.WriteLine(sentence);
+
+        }
+    }
+}
